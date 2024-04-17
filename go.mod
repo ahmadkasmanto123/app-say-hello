6aec5056df32eb54f4f7b3812839066df8b59963
ahmad_kasmanto <ahmad.k@inception.asia>
2024-04-17 16:15:34 +0700

    say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ahmadkasmanto123/app-say-hello.git
+
+go 1.22.1
+
+require github.com/ahmadkasmanto123/go-say-hello v1.1.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/ahmadkasmanto123/go-say-hello"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello())
+}
